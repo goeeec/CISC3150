5395b64251e13c196a4a877acde18cd79b44bc5d
Joey Ching <joeyching1@gmail.com>
2018-02-04 14:34:35 -0500

    added Q4 and Q5

---
+++ Q4_ReadAndPrint.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Q4_ReadAndPrint
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext())
+			System.out.println(input.next());
+	}
+}
