2462819cb6cb8fc566c3f49b1094dc2d35333c5f
Joey Ching <joeyching1@gmail.com>
2018-02-09 22:51:32 -0500

    added Q4

---
+++ HW2/Q4_TokenizeByComma.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Q4_TokenizeByComma
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		input.useDelimiter(",");
+		System.out.println("Please enter string separated by comma for tokenization");
+		while (input.hasNext())
+			System.out.println(input.next());
+	}
+}
+
+/*
+ * useDelimiter method:
+ * https://docs.oracle.com/javase/7/docs/api/
+ */
